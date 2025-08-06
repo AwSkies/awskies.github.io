457c8e0bb7c94b0cf6472345ff3ab6a89ca9362d
Akash Shah <akash.w.shah@gmail.com>
2025-08-05 22:38:52 -0400

    Post search and filtering nearly complete

--- src/blog/PostList.tsx
+++ src/blog/PostList.tsx
@@ -6,45 +6,68 @@ import { useState } from "react";
 
 const SEARCH_PARAM = "search";
 const TAG_PARAM = "tag";
+const TAG_MODE_PARAM = "tagMode"
+const AND = 'and';
+const OR = 'or';
+
+export { SEARCH_PARAM, TAG_PARAM, TAG_MODE_PARAM, AND, OR }
 
 export default function PostList() {
-  let [searchParams, setSearchParams] = useSearchParams({ search: "", tag: [] as tag[] });
-  let [tag, setTag] = useState<tag>('no tag')
+  let [searchParams, setSearchParams] = useSearchParams({ tag: [] as tag[], tagMode: 'and' });
+  let [tagSelection, setTagSelection] = useState<tag>('no tag')
+
+  function editParam(action: (params: URLSearchParams) => void) {
+    const newParams = new URLSearchParams(searchParams);
+    action(newParams);
+    setSearchParams(newParams);
+  }
 
   return (
     <div className={styles.postList}>
       <div className={styles.search}>
-        <label htmlFor={SEARCH_PARAM}>Search</label>
-        <input name={SEARCH_PARAM} value={searchParams.get(SEARCH_PARAM)!} onChange={(e) => {
-          const newParams = new URLSearchParams(searchParams);
-          newParams.set(SEARCH_PARAM, e.target.value);
-          setSearchParams(newParams);
-        }} />
+        {/* Input is put inside a form to execute the search only when you press enter on the search box.
+            This is because updating the search live with each `onChange` call was very slow. */}
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          // Get new value of input box
+          const value = ((e.target as HTMLFormElement)[SEARCH_PARAM] as HTMLInputElement).value;
+          editParam((p) => value === '' ? p.delete(SEARCH_PARAM) : p.set(SEARCH_PARAM, value));
+        }} >
+          <label htmlFor={SEARCH_PARAM}>Search</label>
+          <input name={SEARCH_PARAM} defaultValue={searchParams.get(SEARCH_PARAM) ?? ''} />
+          <input type="submit" value="Search" />
+        </form>
       </div>
       <div className={styles.tagSearch}>
         <label htmlFor={TAG_PARAM}>Tags</label>
-        <select id={TAG_PARAM} name={TAG_PARAM} value={tag} onChange={(e) => {
-          setTag(e.target.value as tag);
-
-        }}>
+        <select id={TAG_PARAM} name={TAG_PARAM} value={tagSelection} onChange={(e) => setTagSelection(e.target.value as tag)}>
           {TAGS.map((tag, i) => <option value={tag} key={i}>{tag}</option>)}
         </select>
         <button onClick={() => {
-          const newParams = new URLSearchParams(searchParams);
-          const currentTags = searchParams.getAll(TAG_PARAM)
-          if (!currentTags.includes(tag)) {
-            newParams.append(TAG_PARAM, tag)
-            setSearchParams(newParams);
+          if (!searchParams.getAll(TAG_PARAM).includes(tagSelection)) {
+            editParam((p) => p.append(TAG_PARAM, tagSelection));
           }
         }
         }>Add tag</button>
+        <div className={styles.tagMode}>
+          <button disabled={searchParams.get(TAG_MODE_PARAM) === AND} onClick={() => editParam((p) => p.set(TAG_MODE_PARAM, AND))}>AND</button>
+          <button disabled={searchParams.get(TAG_MODE_PARAM) === OR} onClick={() => editParam((p) => p.set(TAG_MODE_PARAM, OR))}>OR</button>
+        </div>
       </div>
       <div className={styles.posts}>
         {
           posts.filter( // Filter posts by title search query
-            (post) => post.title.toLowerCase().includes(searchParams.get(SEARCH_PARAM)!.toLowerCase())
+            (post) => post.title.toLowerCase().includes((searchParams.get(SEARCH_PARAM) ?? '').toLowerCase())
           ).filter( // Filter posts by tag search query
-            (post) => post.tags.includes(tag)
+            (post) => {
+              const tags = searchParams.getAll(TAG_PARAM) as tag[];
+              // Bypass this filter if no tag parameters are set
+              if (tags.length === 0) {
+                return true;
+              }
+              const inclusions = tags.map((tag) => post.tags.includes(tag));
+              return searchParams.get(TAG_MODE_PARAM) === AND ? inclusions.every(x => x) : inclusions.some(x => x);
+            }
           ).map( // Map posts to elements
             (post, i) => <PostInfo key={i}>{post}</PostInfo>
           )
