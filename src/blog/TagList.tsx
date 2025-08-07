9ff3d1df3a7760ef2bbfe7b44861c97a7ee542ad
Akash Shah <akash.w.shah@gmail.com>
2025-08-07 15:45:14 -0400

    TagList code formatting

--- src/blog/TagList.tsx
+++ src/blog/TagList.tsx
@@ -4,7 +4,14 @@ import Tag, { TagHandle } from "./Tag";
 import styles from "./TagList.module.css";
 import classNames from "classnames";
 
-export default function TagList({ tags, handle, className, children }: { tags: TagName[], handle?: TagHandle, className?: string, children?: JSX.Element | string }) {
+export default function TagList(
+  { tags, handle, className, children }: {
+    tags: TagName[],
+    handle?: TagHandle,
+    className?: string,
+    children?: JSX.Element | string
+  }
+) {
   return (
     <div className={classNames(styles.tagList, className)}>
       <span>{children ?? <b>Tags:</b>}</span>
