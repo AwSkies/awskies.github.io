9efc9a4b55996f6aed317b645a90d9fe8d1dffe1
Akash Shah <akash.w.shah@gmail.com>
2025-08-07 23:23:12 -0400

    Added date to PostInfo and updated style

--- src/blog/PostInfo.tsx
+++ src/blog/PostInfo.tsx
@@ -6,8 +6,10 @@ import TagList from "./TagList";
 export default function PostInfo({ children }: { children: Post }) {
   return (
     <div className={styles.postInfo}>
-      <div className={styles.info}>
-        <h2><NavLink to={`${POST_PATH}/${children.name}`}>{children.title}</NavLink></h2>
+      <h2><NavLink to={`${POST_PATH}/${children.name}`}>{children.title}</NavLink></h2>
+      <hr />
+      <div className={styles.metadata}>
+        <span>Posted <i>{children.date.toDateString()}</i></span>
         <TagList tags={children.tags} />
       </div>
       <p>{children.description}</p>
